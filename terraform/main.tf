6d00110740c976f6467ca15ff8fc08921e7e942b
Xiaoli Shen <alabebop@gmail.com>
2023-09-06 13:49:25 +0900

    create sandbox1 users

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-module "sandbox" {
+module "sandbox-1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox-1"
 }
+
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "xiaolshe+cttf-sandbox-1@amazon.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "xiaolshe+cttf-sandbox-1@amazon.com"
+#     SSOUserFirstName          = "Sandbox1"
+#     SSOUserLastName           = "AFT"
+#   }
+
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "Xiaoli Shen"
+#     change_reason       = "Demo account provision with AFT"
+#   }
+
+#   custom_fields = {
+#     group = "AWSAccountFactory"
+#   }
+
+#   account_customizations_name = "sandbox-1"
+# }
